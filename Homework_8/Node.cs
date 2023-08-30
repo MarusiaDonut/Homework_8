afec9de866c44ac43a7797c7dceaf85e7d5d7b66
MYudina <MYudina@PC_MSK-20470>
2023-08-30 13:32:24 +0300

    Add project files.

---
+++ Homework_8/Node.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8
+{
+    internal class Node
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+       public void AddNode(Node root, Node toAddSalary, Node toAddName)
+        {
+            if (toAddSalary.Salary < root.Salary)
+            {
+                if (root.Left != null)
+                {
+                    AddNode(root.Left, toAddSalary, toAddName);
+                }
+                else
+                {
+                    root.Left = toAddName;
+                    root.Left = toAddSalary;
+                }
+            }
+            else
+            {
+                if (root.Right != null)
+                {
+                    AddNode(root.Right, toAddSalary, toAddName);
+                }
+                else
+                {
+                    root.Right = toAddSalary;
+                    root.Right = toAddName;
+                }
+            }
+        }
+
+        public Node FindNode(Node root, double number)
+        {
+            if (number < root.Salary)
+            {
+                if (root.Left != null)
+                {
+                    return FindNode(root.Left, number);
+                }
+                return null;
+            }
+            if (number > root.Salary)
+            {
+                if (root.Right != null)
+                {
+                    return FindNode(root.Right, number);
+                }
+                return null;
+            }
+            return root;
+        }
+
+        public void Sort(Node originiNode)
+        {
+
+            if (originiNode.Left != null)
+            {
+                Sort(originiNode.Left);
+            }
+            Console.WriteLine("Имя сотрудника - " + originiNode.Name + ", зарплата - " + originiNode.Salary);
+            if (originiNode.Right != null)
+            {
+                Sort(originiNode.Right);
+            }
+        }
+    }
+}
---
+++ Homework_8/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Homework_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int salary = 0;
+            Node node = new Node();
+            Node root = null;
+            string name = "";
+            do
+            {
+                Console.WriteLine("Введите имя сотрудника.");
+                name = Console.ReadLine();
+                if (name != "")
+                {
+                    Console.WriteLine("Введите зарплату сотрудника.");
+                    if (Int32.TryParse(Console.ReadLine(), out salary))
+                    {
+
+                        if (root == null)
+                        {
+                            root = new Node()
+                            {
+                                Salary = salary,
+                                Name = name
+                            };
+                        }
+                        else
+                        {
+                            node.AddNode(root, new Node
+                            {
+                                Salary = salary,
+                                Name = name
+
+                            }, new Node
+                            {
+                                Salary = salary,
+                                Name = name
+                            });
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите зарплату числом.");
+                    }
+                }
+                else
+                {
+                    break;
+                }
+               
+            }
+            while (name != "");
+
+            node.Sort(root);
+
+            Console.WriteLine("Какой размер зарплаты вас интересует?");
+            while (true)
+            {
+                int salaryFind = 0;
+                if (Int32.TryParse(Console.ReadLine(), out salaryFind))
+                {
+                    if (salaryFind == 0)
+                    {
+                        break;
+                    }
+                    var n = node.FindNode(root, salaryFind);
+                    if (node == null)
+                    {
+                        Console.WriteLine("Такого сотрудника не существует.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Найдена зарплата {n.Salary} у сотрудника {n.Name}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Введите зарплату числом.");
+                }    
+            }
+        }
+    }
+}
